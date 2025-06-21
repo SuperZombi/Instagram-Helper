b60a7fe47702af90239f92574ea896e9d628946a
Super Zombi <super.zombi.yt@gmail.com>
2025-06-21 19:07:12 +0300

    Update insta-sctipt.js

--- insta-sctipt.js
+++ insta-sctipt.js
@@ -12,9 +12,6 @@ window.onload = function() {
 		}
 	})
 }
-function isReels(){
-	return window.location.href.startsWith("https://www.instagram.com/reels/")
-}
 function reels(){
 	let mainMenu = document.querySelector("#insta-helper-menu")
 	if (!mainMenu){
@@ -38,44 +35,6 @@ function reels(){
 		mainMenu.appendChild(speedButton())
 		mainMenu.appendChild(scrollDown())
 	}
-}
-
-
-function scrollInit(icon, direction){
-	let div = document.createElement("div")
-	div.innerHTML = icon
-	Object.assign(div.style, {
-		cursor: "pointer",
-	})
-	let container = document.querySelector("main > div")
-	if (container){
-		div.onclick = _=>{
-			let amount = direction * container.clientHeight;
-			container.scrollBy({
-				top: amount,
-				behavior: 'smooth'
-			});
-		}
-	} else {
-		console.error("Reels container not found!")
-	}
-	return div
-}
-function scrollUp(){
-	return scrollInit(`
-		<svg xmlns="http://www.w3.org/2000/svg" stroke="lightblue" stroke-width="1.5" fill="none" stroke-linecap="round" viewBox="0 0 24 24">
-			<path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Z"/>
-			<path d="m8 13.78 2.87-3.06c.6-.67 1.66-.67 2.26 0L16 13.78"/>
-		</svg>
-	`, -1)
-}
-function scrollDown(){
-	return scrollInit(`
-		<svg xmlns="http://www.w3.org/2000/svg" stroke="lightblue" stroke-width="1.5" fill="none" stroke-linecap="round" viewBox="0 0 24 24">
-			<path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Z"/>
-			<path d="m16 10.22-2.87 3.06a1.54 1.54 0 0 1-2.26 0L8 10.22"/>
-		</svg>
-	`, 1)
 }
 
 
@@ -167,7 +126,53 @@ function speedButton(){
 }
 
 
+
+function scrollInit(icon, direction){
+	let div = document.createElement("div")
+	div.innerHTML = icon
+	Object.assign(div.style, {
+		cursor: "pointer",
+	})
+	let container = document.querySelector("main > div")
+	if (container){
+		div.onclick = _=>{
+			let amount = direction * container.clientHeight;
+			container.scrollBy({
+				top: amount,
+				behavior: 'smooth'
+			});
+		}
+	} else {
+		console.error("Reels container not found!")
+	}
+	return div
+}
+function scrollUp(){
+	return scrollInit(`
+		<svg xmlns="http://www.w3.org/2000/svg" stroke="lightblue" stroke-width="1.5" fill="none" stroke-linecap="round" viewBox="0 0 24 24">
+			<path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Z"/>
+			<path d="m8 13.78 2.87-3.06c.6-.67 1.66-.67 2.26 0L16 13.78"/>
+		</svg>
+	`, -1)
+}
+function scrollDown(){
+	return scrollInit(`
+		<svg xmlns="http://www.w3.org/2000/svg" stroke="lightblue" stroke-width="1.5" fill="none" stroke-linecap="round" viewBox="0 0 24 24">
+			<path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Z"/>
+			<path d="m16 10.22-2.87 3.06a1.54 1.54 0 0 1-2.26 0L8 10.22"/>
+		</svg>
+	`, 1)
+}
+
+
 /* Helpers */
+function getReelId(url) {
+	const match = url.match(/\/reels?\/([^/?#]+)/i);
+	return match ? match[1] : null;
+}
+function isReels(){
+	return window.location.href.startsWith("https://www.instagram.com/reels/")
+}
 function isInViewport(element, percentVisible=100) {
 	const rect = element.getBoundingClientRect();
 	const windowHeight = window.innerHeight || document.documentElement.clientHeight;
