8ba0ec58a3c528c6c821ceaef28f3e784cc24552
Super Zombi <super.zombi.yt@gmail.com>
2025-06-22 18:57:37 +0300

    Fix firefox warning

--- src/insta-sctipt.js
+++ src/insta-sctipt.js
@@ -19,7 +19,7 @@ function reels(){
 		mainMenu.id = "insta-helper-menu"
 		Object.assign(mainMenu.style, {
 			position: "fixed",
-			right: 0,
+			right: "10px",
 			top: 0,
 			bottom: 0,
 			width: "40px",
@@ -30,10 +30,10 @@ function reels(){
 			gap: "10px",
 		})
 		document.body.appendChild(mainMenu)
-		mainMenu.appendChild(scrollUp())
+		mainMenu.appendChild(scrollButton("up"))
 		mainMenu.appendChild(downloadButton())
 		mainMenu.appendChild(speedButton())
-		mainMenu.appendChild(scrollDown())
+		mainMenu.appendChild(scrollButton("down"))
 	}
 }
 
@@ -163,17 +163,33 @@ function speedButton(){
 
 
 
-function scrollInit(icon, direction){
+function scrollButton(direction){
 	let div = document.createElement("div")
-	div.title = direction > 0 ? "Next" : "Previous"
-	div.innerHTML = icon
 	Object.assign(div.style, {
 		cursor: "pointer",
 	})
+	if (direction === "up"){
+		div.title = "Previous"
+		div.innerHTML = `
+			<svg xmlns="http://www.w3.org/2000/svg" stroke="lightblue" stroke-width="1.5" fill="none" stroke-linecap="round" viewBox="0 0 24 24">
+				<path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Z"/>
+				<path d="m8 13.78 2.87-3.06c.6-.67 1.66-.67 2.26 0L16 13.78"/>
+			</svg>
+		`
+	}
+	else if (direction === "down"){
+		div.title = "Next"
+		div.innerHTML = `
+			<svg xmlns="http://www.w3.org/2000/svg" stroke="lightblue" stroke-width="1.5" fill="none" stroke-linecap="round" viewBox="0 0 24 24">
+				<path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Z"/>
+				<path d="m16 10.22-2.87 3.06a1.54 1.54 0 0 1-2.26 0L8 10.22"/>
+			</svg>
+		`
+	} else { console.error(`Wrong direction: ${direction}`) }
 	let container = document.querySelector("main > div")
 	if (container){
 		div.onclick = _=>{
-			let amount = direction * container.clientHeight;
+			let amount = direction === "up" ? -1 : 1 * container.clientHeight;
 			container.scrollBy({
 				top: amount,
 				behavior: 'smooth'
@@ -183,22 +199,6 @@ function scrollInit(icon, direction){
 		console.error("Reels container not found!")
 	}
 	return div
-}
-function scrollUp(){
-	return scrollInit(`
-		<svg xmlns="http://www.w3.org/2000/svg" stroke="lightblue" stroke-width="1.5" fill="none" stroke-linecap="round" viewBox="0 0 24 24">
-			<path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Z"/>
-			<path d="m8 13.78 2.87-3.06c.6-.67 1.66-.67 2.26 0L16 13.78"/>
-		</svg>
-	`, -1)
-}
-function scrollDown(){
-	return scrollInit(`
-		<svg xmlns="http://www.w3.org/2000/svg" stroke="lightblue" stroke-width="1.5" fill="none" stroke-linecap="round" viewBox="0 0 24 24">
-			<path d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20Z"/>
-			<path d="m16 10.22-2.87 3.06a1.54 1.54 0 0 1-2.26 0L8 10.22"/>
-		</svg>
-	`, 1)
 }
 
 
